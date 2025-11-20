c9a28c758c3e009e72cc3730fe25f3e824fcc1eb
OswaldoArceoR <a23216876@alumnos.uady.mx>
2025-11-20 16:25:54 -0600

    Ligeros cambios para el correcto funcionamiento de la pagina con PHP

--- login.php
+++ login.php
@@ -60,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $_SESSION['user_id'] = $row['id'];
     $_SESSION['username'] = $row['username'];
 
-    header("Location: prueba/prueba.html");
+    header("Location: Prueba_PaginaPrincipal/paginaprincipal.html");
     exit();
 }
 ?>
