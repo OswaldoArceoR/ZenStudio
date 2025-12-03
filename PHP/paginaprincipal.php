ca67420a6697be132a2076f2aa137a9cafeb97f5
OswaldoArceoR <a23216876@alumnos.uady.mx>
2025-12-03 12:34:23 -0600

    ya quedó en el mismo estilo el boton de subirarchivos y el de gestionar usuarios

--- PHP/paginaprincipal.php
+++ PHP/paginaprincipal.php
@@ -169,7 +169,7 @@ if ($conexion) {
                     </div>
                 </div>
                     <?php if (!empty($_SESSION['rol']) && $_SESSION['rol'] === 'admin'): ?>
-                    <a href="subirContenido.php" class="action-btn primary-btn" title="Gestionar Contenido" style="margin-left: 10px; display:inline-flex; align-items:center; gap:6px;">
+                    <a href="subirContenido.php" class="action-btn secondary-btn" title="Gestionar Contenido" style="margin-left: 10px; display:inline-flex; align-items:center; gap:6px;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-upload">
                             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                             <polyline points="17 8 12 3 7 8"></polyline>
